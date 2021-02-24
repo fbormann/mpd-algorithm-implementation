74ffec957d02ed6c34bf7d9aa4305d04e8470fb5
bormann <felipe.bormann@gmail.com>
2021-02-23 23:55:00 -0300

    modify code to pretty print policies and add named_directions

--- main.py
+++ main.py
@@ -2,41 +2,61 @@ import copy
 from typing import List, Tuple
 
 import numpy as np
+from pprint import pprint
 
 
 def get_possible_states(state: (int, int), max_rows: int, max_columns: int, goal_states: List[Tuple[int, int]],
-                        is_utility: bool) -> List[
-    Tuple[int, int]]:
+                        is_utility: bool) -> (List[Tuple[int, int]], List[str]):
     possible_states = []
+    named_direction = []
 
     if state in goal_states and is_utility:
-        return possible_states  # return possible state empty for goal states
+        return possible_states, named_direction  # return possible state empty for goal states
 
     if state[0] != 0:
         # it can go up
         possible_states.append((state[0] - 1, state[1]))
+        named_direction.append("up")
 
     if state[1] != 0:
         # it can go left
         possible_states.append((state[0], state[1] - 1))
+        named_direction.append("left")
 
     if state[0] != max_rows - 1:
         # it can go down
         possible_states.append((state[0] + 1, state[1]))
+        named_direction.append("down")
+
     if state[1] != max_columns - 1:
         # it can go right
         possible_states.append((state[0], state[1] + 1))
+        named_direction.append("right")
 
     # index: int = 0
     # while index < len(possible_states) - 1:
     #     if possible_states[index] in goal_states:
     #         del possible_states[index]
     #     index += 1
-    return possible_states
+    return possible_states, named_direction
 
 
-def update_utility_matrix(rw: np.array, value: np.array) -> np.array:
-    pass
+def print_policy(policies, shape, utility_matrix=None, goal_states=None):
+    policies_matrix = []
+    i: int = 0
+    while i < shape[0]:
+        policies_matrix.append([])
+        j: int = 0
+        while j < shape[1]:
+            if (i, j) not in goal_states:
+                policies_matrix[i].append(policies[i, j])
+            else:
+                policies_matrix[i].append(utility_matrix[i, j])
+            j += 1
+
+        i += 1
+
+    return policies_matrix
 
 
 def update_calculations(policies, rw, value: np.array, goal_states: List[Tuple[int, int]]):
@@ -49,7 +69,7 @@ def update_calculations(policies, rw, value: np.array, goal_states: List[Tuple[i
     while i < max_rows:
         j: int = 0
         while j < max_columns:
-            possible_states: List[(int, int)] = get_possible_states((i, j), max_rows, max_columns, goal_states, True)
+            possible_states, named_direction = get_possible_states((i, j), max_rows, max_columns, goal_states, True)
             state_scores = list(map(lambda state: value[state[0], state[1]], possible_states))
             if len(state_scores) > 0:
                 new_value[i, j] = rw[i, j] + state_scores[np.argmax(state_scores)]
@@ -63,9 +83,9 @@ def update_calculations(policies, rw, value: np.array, goal_states: List[Tuple[i
     while i < max_rows:
         j: int = 0
         while j < max_columns:
-            possible_states: List[(int, int)] = get_possible_states((i, j), max_rows, max_columns, goal_states, False)
+            possible_states, named_direction = get_possible_states((i, j), max_rows, max_columns, goal_states, False)
             state_scores = list(map(lambda state: new_value[state[0], state[1]], possible_states))
-            policies[(i, j)] = possible_states[np.argmax(state_scores)]
+            policies[(i, j)] = named_direction[np.argmax(state_scores)]
             j += 1
         i += 1
 
@@ -78,7 +98,7 @@ def update_calculations(policies, rw, value: np.array, goal_states: List[Tuple[i
 
 
 def run_experiments(default_reward: float):
-    goal_states: List[Tuple[int, int]] = [(0, 3), (1, 3)]
+    goal_states: List[Tuple[int, int]] = [(0, 3), (1, 3), (2, 3), (1, 1)]
     # define initial utilities matrix
     matrix: np.array = np.zeros((3, 4))
 
@@ -111,20 +131,20 @@ def run_experiments(default_reward: float):
     rate_of_change = abs(actual_difference) - abs(previous_difference)
     multiple_run = False
     run_amount = 0
-    while (rate_of_change > 0.05 or abs(actual_difference)/abs(sum(sum(utility_matrix))) > 0.05) or not multiple_run:
+    while (rate_of_change > 0.05 or abs(actual_difference) / abs(sum(sum(utility_matrix))) > 0.05) or not multiple_run:
         previous_utility_matrix = copy.deepcopy(utility_matrix)
         policies, utility_matrix = update_calculations(policies, reward_matrix, previous_utility_matrix, goal_states)
         print(sum(sum(previous_utility_matrix)))
         print(sum(sum(utility_matrix)))
         previous_difference = actual_difference
         actual_difference = sum(sum(previous_utility_matrix)) - sum(sum(utility_matrix))
-        rate_of_change = (abs(actual_difference) - abs(previous_difference))/abs(previous_difference)
+        rate_of_change = (abs(actual_difference) - abs(previous_difference)) / abs(previous_difference)
         multiple_run = True
         run_amount += 1
 
     print(f"stabilized matrix after {run_amount} runs")
     print("policies: ")
-    print(policies)
+    pprint(print_policy(policies, utility_matrix.shape, utility_matrix, goal_states))
     print("utility matrix: ")
     print(utility_matrix)
     # utility_matrix: np.array = update_utility_matrix(reward_matrix, utility_matrix)
@@ -133,8 +153,6 @@ def run_experiments(default_reward: float):
 
 
 if __name__ == '__main__':
-    initial_state = (0, 0)  # change it to be a input of a given user.
-
     possible_rewards = [-0.4, -0.04, -0.0004]
     fading_factor = [0.8, 0.3, 0.1, 0.05, 0.0005]
     for chosen_reward in possible_rewards:
